c1ccb7289db5ba6111011b1ddde7b3f469e30f1f
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-12-12 23:57:48 -0800

    description button added

--- effects.js
+++ effects.js
@@ -14,3 +14,14 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+//---PROJECTS PUSH DOWN MENU---//
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
