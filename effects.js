dc5ea7bc90e607183c71345acff1ba0f5a69e746
ofthemeadow <37228366+ofthemeadow@users.noreply.github.com>
2018-12-20 20:20:27 -0800

    prjoects accordion look
    better, newer, more neat look

--- effects.js
+++ effects.js
@@ -1,21 +1,12 @@
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
 //---PROJECTS PUSH DOWN MENU---//
+
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
 
 function myFunction() {
   var x = document.getElementById("myDIV");
